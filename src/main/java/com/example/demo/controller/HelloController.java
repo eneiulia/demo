adfe776c0ca0b50bc47a048d5cce2f5dddcef470
Ene Iulia-Stefania <Iulia-Stefania.Ene@ing.com>
2020-11-13 12:35:22 +0200

    Created project

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/test")
+    public InetAddress test() throws UnknownHostException {
+        return InetAddress.getLocalHost();
+    }
+}
